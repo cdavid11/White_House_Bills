b23c783fb4159c32cab249e425bfa2e97a29d804
cdavid11 <chaddavidson001@gmail.com>
2017-07-12 02:45:15 -0400

    Slowly getting to working order

--- WH_scrape/spiders/White_House_Bills.py
+++ WH_scrape/spiders/White_House_Bills.py
@@ -7,37 +7,49 @@ from auth import auth
 
 class Bill_Spider (scrapy.Spider):
 
-        name = "White_House_Bills"
+                name = "White_House_Bills"
 
-        def start_requests (self):
+                def start_requests (self):
 
-                url = "https://www.whitehouse.gov/briefing-room/signed-legislation"
-                yield scrapy.Request(url=url, callback=self.parse)
+                    url = "https://www.whitehouse.gov/briefing-room/signed-legislation"
+                    yield scrapy.Request(url=url, callback=self.parse, meta={"sorv":"s"})
+                    url = "https://www.whitehouse.gov/briefing-room/vetoed-legislation"
+                    yield scrapy.Request(url=url, callback=self.parse, meta={"sorv":"v"})
 
-        def parse (self, response):
+                def parse (self, response):
 
-                soup = BeautifulSoup(response.body, 'html.parser')
-                panel = soup.find("div", {"class":"view-content"})
-                bill = panel.find("div", {"class":"views-row views-row-1 views-row-odd views-row-first"})
+                    sorv = response.meta.get('sorv')
+                    print("SORV: " + sorv)
 
-                name = bill.a.text
-                link = "https://www.whitehouse.gov" + bill.a['href']
-                print(name)
-                print(link)
+                    soup = BeautifulSoup(response.body, 'html.parser')
+                    panel = soup.find("div", {"class":"view-content"})
+                    if (panel == None):
+                        print("Nothing here")
+                        return
+                    bill = panel.find("div", {"class":"views-row views-row-1 views-row-odd views-row-first"})
+
+                                
+                    name = bill.a.text
+                    link = "https://www.whitehouse.gov" + bill.a['href']
+                    print(name)
+                    print(link)
 
 
-                if (len(name) > 91):
+                    if (len(name) > 91):
                         diff = len(name) - 91
                         name = name[0:diff-3]
                         name = name + "..."
-               
-                to_tweet = "@POTUS has signed: " + name +link + "\n#Trump"
+                    
+                    if (sorv == "s"):         
+                        to_tweet = "@POTUS has signed: " + name +link + "\n#Trump"
+                    else:
+                        to_tweet = "@POTUS has vetoed: " + name +link + "\n#Trump"
+                    a = auth()
 
-                a = auth()
-
-                print(len(to_tweet))
-
-
-               	api = twitter.Api(a.consumer_key, a.consumer_secret, a.access_token_key, a.access_token_secret)
-               	status = api.PostUpdate(to_tweet)
-               	print(status.text)
+                    try:
+                        api = twitter.Api(a.consumer_key, a.consumer_secret, a.access_token_key, a.access_token_secret)
+                        status = api.PostUpdate(to_tweet)
+                        print(status.text)
+                    except:
+                        print("Tweeting Error")
+                        return
