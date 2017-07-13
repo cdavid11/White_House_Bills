7a12690ec960eb864e2973d03e4999f180eb95b7
cdavid11 <chaddavidson001@gmail.com>
2017-07-13 02:38:32 -0400

    Comments

--- auth.py
+++ auth.py
@@ -1,3 +1,6 @@
+#Simple, place your authorization information here. Obviously don't commit this file with that info in it.
+#Just instantiate auth and use it with the python-twitter library (or any of the others really).
+
 class auth:
 
 	def __init__ (self):
